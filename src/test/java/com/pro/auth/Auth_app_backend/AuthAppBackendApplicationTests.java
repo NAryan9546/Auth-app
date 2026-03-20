ab80c631eacc9428f233be6b638c77dcf6539288
NAryan9546 <aryanalhawat1990@gmail.com>
2026-03-20 11:43:52 +0530

    commit

---
+++ src/test/java/com/pro/auth/Auth_app_backend/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.auth.Auth_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
